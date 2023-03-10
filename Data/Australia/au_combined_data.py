c1178032519729bb29dddb1d9f623641ada865b4
Peter <pnpoliakov@gmail.com>
2023-03-10 04:50:22 -0800

    first combinations + fixed interest rate data

--- Data/Australia/au_combined_data.py
+++ Data/Australia/au_combined_data.py
@@ -2,13 +2,26 @@ import os
 import pandas as pd
 
 
-def australia_process_file(dir: str) -> pd.DataFrame:
+def congregate(dir: str) -> pd.DataFrame:
+    filenames = os.listdir(dir)
+    fname_pd_dict: dict[str, pd.DataFrame] = {}
+    for filename in filenames:
+        pathname = os.path.join(dir, filename)
+        if os.path.isfile(pathname):
+            if os.path.splitext(pathname)[1] == ".csv":
+                fname_pd_dict[filename] = pd.read_csv(pathname)
+
+    interest_rates = fname_pd_dict['au_quarterly_interest_rate.csv']
+    merged_interest_rates = interest_rates.groupby('TIME')["Value"].mean()
+    merged_interest_rates = merged_interest_rates.reset_index()
+
+    cpi = fname_pd_dict[]
     pass
 
 
 def main():
     directory = ".Data/Australia"
-    australia = australia_process_file(directory)
+    australia = congregate(directory)
     print(australia)
     pass
 
